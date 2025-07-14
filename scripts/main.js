135589fcad8bb3b196da31c370c5dc59a0cce461
Pedro Sousa <pedrovictorsousa69@gmail.com>
2025-07-14 01:02:57 -0300

    ajustes nav-menu mobile

--- scripts/main.js
+++ scripts/main.js
@@ -12,25 +12,38 @@ const navMenu = document.querySelector('.nav-menu');
 const icon = menuToggle.querySelector('i');
 const header = document.querySelector('.header');
 
-  menuToggle.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 
-    if (navMenu.classList.contains('active')) {
+    const isMenuOpen = navMenu.classList.contains('active');
+
+    header.classList.add('no-transition');
+
+    if (isMenuOpen) {
         icon.classList.remove('bi-list');
         icon.classList.add('bi-x');
+        header.classList.add('scrolled');
     } else {
         icon.classList.remove('bi-x');
         icon.classList.add('bi-list');
+
+        if (window.scrollY === 0) {
+        header.classList.remove('scrolled');
+        }
     }
 
+    setTimeout(() => {
+        header.classList.remove('no-transition');
+    }, 10);
 });
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
         header.classList.add('scrolled');
-    } else {
+    } else if (!navMenu.classList.contains('active')) {
+        // Só remove se o menu estiver fechado
         header.classList.remove('scrolled');
-    }
+    }   
 });
 
 
